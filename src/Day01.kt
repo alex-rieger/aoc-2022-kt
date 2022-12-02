3b63d4110a529b8e551f56499b3a575ce7e8a30d
Alexander Rieger <alexander.rieger@mail.schwarz>
2022-12-02 12:26:34 +0100

    day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,28 @@
+fun getCaloriesPerElveList(input: List<String>): List<Int> {
+    var buffer = 0
+    return input.fold(mutableListOf<Int>()) { acc, calories ->
+        if (calories.isEmpty()) {
+            acc.add(buffer)
+            buffer = 0
+        } else {
+            buffer = buffer.plus(calories.toInt())
+        }
+        acc
+    }
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return getCaloriesPerElveList(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return getCaloriesPerElveList(input).sorted().takeLast(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    // test if implementation meets criteria from the description, like:
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
